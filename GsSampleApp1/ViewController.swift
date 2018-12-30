851ff7a36debf84407112a18f22582400f8ba2e8
jollyjoester <jollyjoester@gmail.com>
2018-12-30 23:41:08 +0900

    Add initial project.

---
+++ GsSampleApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GsSampleApp1
+//
+//  Created by jollyjoester on 2018/12/30.
+//  Copyright © 2018 jollyjoester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
